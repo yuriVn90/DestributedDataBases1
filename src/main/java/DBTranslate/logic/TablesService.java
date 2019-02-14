4bdd732bd4eb7515cdbfc289ae4a7cea7b145a8f
YURI VAINSTAIN <yuri.vainstain@ibm.com>
2019-02-14 16:12:40 +0200

    added methods to TablesInterface

--- src/main/java/DBTranslate/logic/TablesService.java
+++ src/main/java/DBTranslate/logic/TablesService.java
@@ -1,5 +1,36 @@
 package DBTranslate.logic;
 
+import DBTranslate.DTO.ISqlTableDTO;
+
 public interface TablesService {
+	
+	/**
+	 * @param collectionName
+	 * @return array of collections' records
+	 */
+	public ISqlTableDTO[] getMongoCollection(String collectionName);
+	
+	/**
+	 * 
+	 * @param collectionName
+	 * @param id
+	 * @return the deleted record
+	 */
+	public ISqlTableDTO deleteRecord(String collectionName, int id);
+	
+	/**
+	 * 
+	 * @param collectionName
+	 * @param record
+	 * @return the added record
+	 */
+	public ISqlTableDTO addRecord(String collectionName, ISqlTableDTO record);
+	
+	/**
+	 * 
+	 * @return employees collection sorted by name
+	 */
+	public ISqlTableDTO[] sortEmployeesCollectionByName();
+	
 
 }
