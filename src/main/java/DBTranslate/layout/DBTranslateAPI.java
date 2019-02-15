670a593af0e1b3cc13aeaa0db81d7def853adb39
YURI VAINSTAIN <yuri.vainstain@ibm.com>
2019-02-15 14:04:38 +0200

    added tableService autowire

--- src/main/java/DBTranslate/layout/DBTranslateAPI.java
+++ src/main/java/DBTranslate/layout/DBTranslateAPI.java
@@ -1,5 +1,20 @@
 package DBTranslate.layout;
 
-public class DBTranslateAPI {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+import DBTranslate.logic.TablesService;
+
+@RestController
+public class DBTranslateAPI {
+	
+	private TablesService tables;
+	
+	@Autowired
+	public void setTables(TablesService tables) {
+		this.tables = tables;
+	}
+	
+	
+	
 }
